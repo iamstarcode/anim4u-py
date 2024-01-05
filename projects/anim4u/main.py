e56668693f0b8534d52c7b912d940281fe3bdfe5
Bakare Abiola <iamstarcode@gmail.com>
2024-01-05 21:46:30 +0100

    feat: add poetry

---
+++ projects/anim4u/main.py
@@ -0,0 +1,17 @@
+import typer
+from typing_extensions import Annotated
+
+# from commands import download
+
+app = typer.Typer()
+
+## app.add_typer(download.app, name="download")
+
+
+@app.command()
+def download(query: str):
+    print(f"Creating user: {query}")
+
+
+if __name__ == "__main__":
+    app()
