12c3a783c84fa95c036ec1f4225b758c35fc0dd0
Bakare Abiola <iamstarcode@gmail.com>
2024-01-06 11:41:47 +0100

    feat: added options

--- anim4u-py/main.py
+++ anim4u-py/main.py
@@ -3,13 +3,25 @@ from typing_extensions import Annotated
 from foru_lib_py import cli
 from typing_extensions import Annotated
 
-
 app = typer.Typer()
 
 
 @app.command()
-def download(query: str):
-    print(f"Creating user: {query}", cli.provider_seperated(query))
+def download(
+    query: str,
+    q: Annotated[int, typer.Option()] = None,
+    d: Annotated[bool, typer.Option()] = False,
+    f: Annotated[bool, typer.Option()] = False,
+):
+    [provider, que] = cli.provider_seperated(query)
+    Options = {"provider": provider, "query": que, "quality": q, "force": f}
+    if d:
+        print(f"Options: {Options}")
+
+
+@app.command()
+def clear(username: str):
+    print(f"Deleting user: {username}")
 
 
 if __name__ == "__main__":
