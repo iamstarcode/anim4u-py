f3365f11128fc31bef0c0ad03b2893c761e4aee0
Bakare Abiola <iamstarcode@gmail.com>
2024-01-06 11:04:13 +0100

    chore: setup monorepo

--- anim4u-py/main.py
+++ anim4u-py/main.py
@@ -1,17 +1,15 @@
 import typer
 from typing_extensions import Annotated
+from foru_lib_py import cli
+from typing_extensions import Annotated
 
-
-# from commands import download
 
 app = typer.Typer()
-
-## app.add_typer(download.app, name="download")
 
 
 @app.command()
 def download(query: str):
-    print(f"Creating user: {query}")
+    print(f"Creating user: {query}", cli.provider_seperated(query))
 
 
 if __name__ == "__main__":
---
+++ foru_lib_py/foru_lib_py/cli.py
@@ -0,0 +1,6 @@
+def add():
+    print(2 + 2)
+
+
+def provider_seperated(query: str):
+    return query.split(":")
