257a61aff0864a276a486f2b1e7b316141043b6b
Bakare Abiola <iamstarcode@gmail.com>
2024-01-11 15:22:08 +0100

    chore: created base_provider

--- anim4u-py/providers/base_provider.py
+++ anim4u-py/providers/base_provider.py
@@ -1,4 +1,6 @@
 class BaseProvider:
-    def __init__(self, options) -> None:
+    def __init__(self, options, query, provider, search_path) -> None:
         self.options = options
-        # self.query
+        self.query = query
+        self.provider = provider
+        self.searchPath = search_path
